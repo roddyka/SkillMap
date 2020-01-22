ecf928ee7535b27fdd3c49d766c232eb7c1e84ce
Rodrigo Antunes <rodrigoka@live.com>
2020-01-22 11:54:39 +0000

    actual project

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require('cors');
 
 const app = express();
 
@@ -8,11 +9,12 @@ const app = express();
 
 //conexao com o banco de dados mongodb
 //senha padrao (lembrar)
-mongoose.connect("mongodb+srv://rodrigo:SENHA@cluster0-okzyc.mongodb.net/test?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://rodrigo:A34144304a@cluster0-okzyc.mongodb.net/test?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
